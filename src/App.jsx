388df478c9f15589ff8ba8484d07ac54bd89a142
khantpyaehtoo <ericrebillet@gmail.com>
2026-03-28 22:10:37 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+function App() {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-lg">
+                <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label
+                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            htmlFor="grid-first-name"
+                        >
+                            Full Name
+                        </label>
+                        <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                            id="grid-first-name"
+                            type="text"
+                            placeholder="Jane"
+                        />
+                        <p className="text-red-500 text-xs italic">
+                            Please fill out this field.
+                        </p>
+                    </div>
+                    <div className="w-full md:w-1/2 px-3">
+                        <label
+                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            htmlFor="grid-last-name"
+                        >
+                            Phone Number
+                        </label>
+                        <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-last-name"
+                            type="text"
+                            placeholder="09 xxxxxxxxx"
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full px-3">
+                        <label
+                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            htmlFor="grid-password"
+                        >
+                            Delivery Address
+                        </label>
+                        <textarea
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-password"
+                            type="password"
+                            placeholder="Enter Your Full Address"
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-2">
+                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                        <label
+                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            htmlFor="grid-state"
+                        >
+                            Payment
+                        </label>
+                        <div className="relative">
+                            <select
+                                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-state"
+                            >
+                                <option>Kpay</option>
+                                <option>Wave</option>
+                                <option>AYA</option>
+                            </select>
+                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <svg
+                                    className="fill-current h-4 w-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                >
+                                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-2/3 px-3 mb-6 md:mb-0">
+                        <label
+                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            htmlFor="grid-zip"
+                        >
+                            Last 5 Digit of Payslip Id
+                        </label>
+                        <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-zip"
+                            type="text"
+                            placeholder="90210"
+                        />
+                    </div>
+                </div>
+                <div className="md:flex md:items-center my-4 items-center">
+                    <div className="w-full">
+                        <button
+                            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-full"
+                            type="button"
+                        >
+                            Confirm Payment & Order
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default App;
